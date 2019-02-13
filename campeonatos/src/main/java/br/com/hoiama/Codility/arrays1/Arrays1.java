a543f675c6e9f82a5625cd26929228a77f389981
Hoiama Rosa Rodrigues Costa <hoiama9@gmail.com>
2019-02-13 00:26:20 -0200

    update sintatica

--- campeonatos/src/main/java/br/com/hoiama/Codility/arrays1/Arrays1.java
+++ campeonatos/src/main/java/br/com/hoiama/Codility/arrays1/Arrays1.java
@@ -3,26 +3,19 @@ package br.com.hoiama.Codility.arrays1;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 public class Arrays1 {
     public int solution(int[] A){
        List<Integer> lista = Arrays.stream(A).boxed().collect(Collectors.toList());
-
-       lista.stream().forEach(element -> {
-             long quantidadeElements =  lista
-                   .stream()
-                   .filter(elementoFilter -> elementoFilter.equals(element))
-                   .count();
-             if(quantidadeElements == 1){
-               return quantidadeElements;
-             }
-        });
-
-        return 0;
+        return lista
+               .stream()
+               .filter(element -> {
+                   return Long.valueOf(1).equals(lista
+                            .stream()
+                            .filter(elementoFilter -> elementoFilter.equals(element))
+                            .count());
+               }).mapToInt(i -> i)
+                 .findFirst().getAsInt();
     }
 }
-// comparar cada elemento com os demais da lista verificando repetições
-// caso não encontre repedição retonar o elemento unico.
-
-// https://javarevisited.blogspot.com/2012/11/4-ways-to-search-object-in-java-array-example.html
